2d648703e5afab240bb4a55ada6d3c695b0fb79a
Kayode Dan Precious <danielkayode222@gmail.com>
2025-09-03 12:16:26 +0100

    pricing and about page structure done

---
+++ src/app/_components/sections/pageContainer.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React, { ReactNode } from "react";
+import { FaFacebook } from "react-icons/fa";
+
+const PageContainer: React.FC<{
+  id: string;
+  title: string;
+  path: string;
+  children: ReactNode;
+}> = ({ id, title, path, children }) => {
+  const pathname = usePathname();
+
+  console.log(pathname);
+
+  return (
+    <section className="lg:mx-16">
+      <div className="py-5 bg-foreground/5 border-sub-side px-6 border-sub-bottom flex justify-between items-center">
+        <div className="flex items-center gap-4 text-sm opacity-50">
+          <span className="">Home</span> <span className="">/</span>
+          {path}
+        </div>
+        <div className="">
+          <ul className="flex gap-6">
+            <li className="flex">
+              <Link
+                href=""
+                className="rounded-full p-3 text-[1.5rem] border-sub"
+              >
+                <FaFacebook />
+              </Link>
+            </li>
+            <li className="flex">
+              <Link
+                href=""
+                className="rounded-full p-3 text-[1.5rem] border-sub"
+              >
+                <FaFacebook />
+              </Link>
+            </li>
+            <li className="flex">
+              <Link
+                href=""
+                className="rounded-full p-3 text-[1.5rem] border-sub"
+              >
+                <FaFacebook />
+              </Link>
+            </li>
+            <li className="flex">
+              <Link
+                href=""
+                className="rounded-full p-3 text-[1.5rem] border-sub"
+              >
+                <FaFacebook />
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="border-sub-side">{children}</div>
+    </section>
+  );
+};
+
+export default PageContainer;
