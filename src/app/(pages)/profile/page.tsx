cd701d7492bf35fd72d967f609ad327343e898c0
Kayode Dan Precious <danielkayode222@gmail.com>
2025-09-06 12:06:21 +0100

    profile dashboard ui done

--- src/app/(pages)/profile/page.tsx
+++ src/app/(pages)/profile/page.tsx
@@ -2,15 +2,16 @@ import ProfileSectionsContainer, {
   ProfileDetails,
   ProfileItem,
 } from "@/app/_components/sections/profileSectionsContainer";
+import SavedArticles from "@/app/_components/sections/savedArticles";
 import React from "react";
 
 const ProfilePage = () => {
   return (
-    <section className="flex">
-      <div className="lg:w-[20%] lg:flex hidden">
+    <section className="flex lg:mx-16 mx-3 py-10 lg:py-0">
+      <div className="lg:w-[24%] lg:flex hidden border-sub-side relative">
         <ProfileDetails />
       </div>
-      <div className="w-[80%] p-6 flex flex-col gap-8">
+      <div className="lg:w-[76%] lg:p-6 flex flex-col gap-8 ">
         <ProfileSectionsContainer title="Your Personal Information">
           <ProfileItem editButton={true} title="Title" value="John Doe" />
           <ProfileItem
@@ -34,10 +35,10 @@ const ProfilePage = () => {
             title="By Mail"
             value="Contact our customer service department by email at customercare@gmail.com"
           />
+        </ProfileSectionsContainer>
 
-          <ProfileSectionsContainer title="Saved Articles">
-            <div className=""></div>
-          </ProfileSectionsContainer>
+        <ProfileSectionsContainer title="Saved Articles">
+          <SavedArticles />
         </ProfileSectionsContainer>
       </div>
     </section>
--- src/app/_components/sections/profileSectionsContainer.tsx
+++ src/app/_components/sections/profileSectionsContainer.tsx
@@ -10,9 +10,9 @@ const ProfileSectionsContainer: React.FC<{
   return (
     <section className="rounded-2xl  p-5 bg-foreground/5">
       <div className="pb-6 border-sub-bottom">
-        <h1 className="lg:text-[3.5rem] text-[2rem] font-bold">{title}</h1>
+        <h1 className=" lg:text-[1.7rem] text-[1.3rem] font-bold">{title}</h1>
       </div>
-      <div className="">{children}</div>
+      <div className="flex flex-col gap-7 mt-8 relative">{children}</div>
     </section>
   );
 };
@@ -29,24 +29,34 @@ export const ProfileItem: React.FC<{
   return (
     <div className="flex flex-col w-full">
       <div className="flex justify-between items-center">
-        <div className="flex flex-col gap-5">
+        <div className={`flex flex-col ${editButton ? "gap-5" : "gap-2"} `}>
           <h6 className="">{title}</h6>
           <p className="">{value}</p>
         </div>
         {editButton && (
           <div className="">
-            <button className="btn-var1" type="button">
-              {editButton ? "Change" : "Cancel"}
+            <button
+              onClick={() => setEditActive(!editActive)}
+              className="btn-var1 cursor-pointer"
+              type="button"
+            >
+              {!editActive ? "Change" : "Cancel"}
             </button>
           </div>
         )}
       </div>
       <div className="">
         {editActive && (
-          <div className="py-4">
+          <div className="py-4 transition-all duration-300">
             <form className="">
-              <input type="text" className="input" />
-              <button className="mt-3 btn-var1">Submit</button>
+              <input
+                type="text"
+                className="input"
+                placeholder={`Input your new ${title}`}
+              />
+              <div className="lg:w-[20%]">
+                <button className="mt-3 btn-var1 cursor-pointer">Submit</button>
+              </div>
             </form>
           </div>
         )}
@@ -80,20 +90,34 @@ export const ProfileDetails = () => {
   ];
 
   return (
-    <div className="">
-      <div className=""></div>
-      <div className="border-sub-top">
-        {navLinks.map(({ href, id, title }) => {
-          return (
-            <Link
-              key={id}
-              href={href}
-              className="text-center py-2 w-full border-sub-bottom hover:bg-foreground/5"
-            >
-              {title}
-            </Link>
-          );
-        })}
+    <div className="w-full min-h-[100vh] relative">
+      <div className="sticky top-30">
+        <div className="py-5 flex flex-col gap-3 items-center justify-center">
+          <div className="rounded-full h-15 w-15 bg-foreground/5">
+            {/* Image */}
+          </div>
+
+          {/* Profile name */}
+          <h3 className="font-semibold text-[1.5rem]">John Doe</h3>
+
+          {/* User Status Tag */}
+          <div className="rounded-lg p-3 border-sub text-xs">
+            Non Subscriber
+          </div>
+        </div>
+        <div className="border-sub-top grid w-full">
+          {navLinks.map(({ href, id, title }) => {
+            return (
+              <Link
+                key={id}
+                href={href}
+                className="text-center py-4 text-sm w-full border-sub-bottom hover:bg-foreground/5 hover:border-l-2 hover:border-l-accent-main"
+              >
+                {title}
+              </Link>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
